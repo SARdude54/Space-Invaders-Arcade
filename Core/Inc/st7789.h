bca9a520bb7f6b86e5a4a34f0390e45ea6d06630
Saul Rodriguez <saulargz35@gmail.com>
2025-06-03 20:04:55 -0700

    full screen code

---
+++ Core/Inc/st7789.h
@@ -0,0 +1,28 @@
+/*
+ * st7789.h
+ *
+ *  Created on: Jun 3, 2025
+ *      Author: Saula
+ */
+
+#ifndef ST7789_H
+#define ST7789_H
+
+#include "stm32l4xx_hal.h" // HAL_SPI_Transmit
+
+#define WIDTH  205
+#define HEIGHT 832
+
+// Public functions
+void ST7789_Init(void);
+void ST7789_Reset(void);
+void ST7789_WriteCommand(uint8_t cmd);
+void ST7789_WriteData(uint8_t *data, uint16_t size);
+void ST7789_SetAddrWindow(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1);
+void FillScreenRed(void);
+void FillScreenBlack(void);
+void FillScreenYellow(void);
+void FillScreenGreen(void);
+void FillSCreenColor(uint16_t color);
+
+#endif // ST7789_H
---
+++ Core/Src/st7789.c
@@ -0,0 +1,138 @@
+#include "st7789.h"
+
+// These macros should match your wiring and GPIO setup
+#define CS_LOW()    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET)
+#define CS_HIGH()   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET)
+#define DC_LOW()    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET)
+#define DC_HIGH()   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET)
+#define RST_LOW()   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET)
+#define RST_HIGH()  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_SET)
+
+extern SPI_HandleTypeDef hspi1;
+
+
+/*
+ * Core LCD functionality
+ * */
+
+
+void ST7789_WriteCommand(uint8_t cmd) {
+    DC_LOW();
+    CS_LOW();
+    HAL_SPI_Transmit(&hspi1, &cmd, 1, HAL_MAX_DELAY);
+    CS_HIGH();
+}
+
+void ST7789_WriteData(uint8_t *data, uint16_t size) {
+    DC_HIGH();
+    CS_LOW();
+    HAL_SPI_Transmit(&hspi1, data, size, HAL_MAX_DELAY);
+    CS_HIGH();
+}
+
+void ST7789_Reset() {
+    RST_LOW();
+    HAL_Delay(10);
+    RST_HIGH();
+    HAL_Delay(120);
+}
+
+void ST7789_Init() {
+    ST7789_Reset();
+
+    ST7789_WriteCommand(0x36); // MADCTL
+    uint8_t madctl = 0x00;
+    ST7789_WriteData(&madctl, 1);
+
+    ST7789_WriteCommand(0x3A); // COLMOD
+    uint8_t colmod = 0x55;     // 16-bit/pixel
+    ST7789_WriteData(&colmod, 1);
+
+    ST7789_WriteCommand(0x21); // INVON (optional)
+    ST7789_WriteCommand(0x11); // SLPOUT
+    HAL_Delay(120);
+    ST7789_WriteCommand(0x29); // DISPON
+}
+
+void ST7789_SetAddrWindow(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1) {
+    uint8_t data[4];
+
+    ST7789_WriteCommand(0x2A); // CASET
+    data[0] = x0 >> 8;
+    data[1] = x0 & 0xFF;
+    data[2] = x1 >> 8;
+    data[3] = x1 & 0xFF;
+    ST7789_WriteData(data, 4);
+
+    ST7789_WriteCommand(0x2B); // RASET
+    data[0] = y0 >> 8;
+    data[1] = y0 & 0xFF;
+    data[2] = y1 >> 8;
+    data[3] = y1 & 0xFF;
+    ST7789_WriteData(data, 4);
+
+    ST7789_WriteCommand(0x2C); // RAMWR
+}
+
+/*
+ * Fill Background screen functions in RGB565 format
+ * */
+
+void FillScreenBlack() {
+    uint16_t color = 0x0000;
+    ST7789_SetAddrWindow(0, 0, WIDTH - 1, HEIGHT - 1);
+
+    for (uint32_t i = 0; i < WIDTH * HEIGHT; ++i) {
+        uint8_t data[2] = {color >> 8, color & 0xFF};
+        ST7789_WriteData(data, 2);
+    }
+}
+
+void FillScreenRed() {
+    uint16_t color = 0xF800;
+    ST7789_SetAddrWindow(0, 0, WIDTH - 1, HEIGHT - 1);
+
+    for (uint32_t i = 0; i < WIDTH * HEIGHT; ++i) {
+        uint8_t data[2] = {color >> 8, color & 0xFF};
+        ST7789_WriteData(data, 2);
+    }
+}
+
+void FillScreenBlue() {
+    uint16_t color = 0x001f;
+    ST7789_SetAddrWindow(0, 0, WIDTH - 1, HEIGHT - 1);
+
+    for (uint32_t i = 0; i < WIDTH * HEIGHT; ++i) {
+        uint8_t data[2] = {color >> 8, color & 0xFF};
+        ST7789_WriteData(data, 2);
+    }
+}
+
+void FillScreenGreen() {
+    uint16_t color = 0x07e0;
+    ST7789_SetAddrWindow(0, 0, WIDTH - 1, HEIGHT - 1);
+
+    for (uint32_t i = 0; i < WIDTH * HEIGHT; ++i) {
+        uint8_t data[2] = {color >> 8, color & 0xFF};
+        ST7789_WriteData(data, 2);
+    }
+}
+
+void FillScreenYellow() {
+    uint16_t color = 0xffe0;
+    ST7789_SetAddrWindow(0, 0, WIDTH - 1, HEIGHT - 1);
+
+    for (uint32_t i = 0; i < WIDTH * HEIGHT; ++i) {
+        uint8_t data[2] = {color >> 8, color & 0xFF};
+        ST7789_WriteData(data, 2);
+    }
+}
+
+void FillScreenColor(uint16_t color) {
+    ST7789_SetAddrWindow(0, 0, WIDTH - 1, HEIGHT - 1);
+
+    for (uint32_t i = 0; i < WIDTH * HEIGHT; ++i) {
+        uint8_t data[2] = {color >> 8, color & 0xFF};
+        ST7789_WriteData(data, 2);
+    }
+}
